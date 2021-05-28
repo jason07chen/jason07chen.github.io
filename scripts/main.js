9b3736478cd9c3398abc9404d0fc2d105083cbf5
jason07chen <84947313+jason07chen@users.noreply.github.com>
2021-05-28 10:26:37 +0800

    第一版

---
+++ scripts/main.js
@@ -0,0 +1,30 @@
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc == 'images/Google.jpg') {
+      myImage.setAttribute ('src','images/Firefox.png');
+    } else {
+      myImage.setAttribute ('src','images/Google.jpg');
+    }
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  let myName = prompt('Please enter your name.'); //對話視窗函式
+  localStorage.setItem('name', myName);
+  myHeading.innerHTML = 'Hello, ' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  let storedName = localStorage.getItem('name');
+  myHeading.innerHTML = 'Mozilla is cool, ' + storedName;
+}
+
+myButton.onclick = function() {
+  setUserName();
+}
